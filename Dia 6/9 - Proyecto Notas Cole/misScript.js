eb866cf4a944def162728fd82a8872921775ee2c
Miguel DE LA ROSA PAULET <miguel.delarosa@alten.es>
2023-12-18 19:34:48 +0100

    Dia 6 y comienzo Proyecto 6

---
+++ Dia 6/9 - Proyecto Notas Cole/misScript.js
@@ -0,0 +1,26 @@
+misNotas = [3, 7, 4, 8, 10];
+
+// for of
+function mostrarLista(){
+
+
+
+}
+
+// for
+function calcularPromedio(){
+
+
+}
+
+// while
+function notaMasAlta(){
+
+
+}
+
+// do while
+function hayAplazos(){
+
+
+}
